af5a8e2e1ecd0f568d559be426a3201fc045ed61
a.tarakanov <linedot.www@gmail.com>
2017-12-08 14:21:30 +0300

    add: sass

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var pngquant = require('imagemin-pngquant');
 var MozJpeg = require('imagemin-mozjpeg');
 var gifsicle = require('imagemin-gifsicle');
 var imageminSvgo = require('imagemin-svgo');
+var mqPacker = require('css-mqpacker');
 var plugins = gulpLoadPlugins();
 var browserSync = require('browser-sync').create();
 
@@ -24,48 +25,36 @@ gulp.task('webserver', () => {
 });
 
 // build project
-gulp.task('clean:build', () => {
-	return gulp.src(config.src.build.dest)
-		.pipe(plugins.rimraf());
-});
-
 gulp.task('build', () => {
 
-	gulp.start('clean:build');
-
 	gulp.src(config.src.dev.files)
-		.pipe(gulp.dest(config.src.build.dest))
-
+	  .pipe(gulp.dest(config.src.build.dest))
 
 });
 
 gulp.task('js', () => {
 
 	gulp.src(config.src.source.js.src)
-		.pipe(plugins.plumber({
-			errorHandler: (err) => {
-				console.log(err)
-			}
-		}))
-		.pipe(plugins.sourcemaps.init())
-		.pipe(plugins.concat('script.js'))
-		.pipe(plugins.rename('script.min.js'))
-		.pipe(plugins.if(config.compress.js, plugins.uglify()))
-		.pipe(plugins.sourcemaps.write('.'))
-		.pipe(gulp.dest(config.src.dev.js.dest))
-		.on('end', browserSync.reload);
+	  .pipe(plugins.plumber({
+		  errorHandler: (err) => {
+			  console.log(err)
+		  }
+	  }))
+	  .pipe(plugins.sourcemaps.init())
+	  .pipe(plugins.concat('script.js'))
+	  .pipe(plugins.rename('script.min.js'))
+	  .pipe(plugins.if(config.compress.js, plugins.uglify()))
+	  .pipe(plugins.sourcemaps.write('.'))
+	  .pipe(gulp.dest(config.src.dev.js.dest))
+	  .on('end', browserSync.reload);
 
 	gulp.src(config.src.source.js.copy)
-		.pipe(gulp.dest(config.src.dev.js.dest));
+	  .pipe(gulp.dest(config.src.dev.js.dest));
 
 });
 
 gulp.task('css', () => {
 
-	var importAt = require('postcss-import');
-	var nested = require('postcss-nested');
-	var mixins = require('postcss-mixins');
-	var simpleVars = require('postcss-simple-vars');
 	var cssnano = require('cssnano');
 	var inlineSvg = require('postcss-inline-svg');
 	var svgo = require('postcss-svgo');
@@ -73,11 +62,7 @@ gulp.task('css', () => {
 	var imageInliner = require('postcss-image-inliner');
 
 	var processors = [
-		importAt,
-		mixins,
 		cssnext,
-		nested,
-		simpleVars,
 		inlineSvg({
 			path: config.src.source.svg.dest,
 			removeFill: true
@@ -86,22 +71,24 @@ gulp.task('css', () => {
 			assetPaths: ['https://icongr.am']
 		}),
 		svgo,
+		mqPacker,
 		cssnano({
 			'zindex': false
 		})
 	];
 
 	return gulp.src(config.src.source.css.src)
-		.pipe(plugins.plumber({
-			errorHandler: (err) => {
-				console.log(err)
-			}
-		}))
-		.pipe(plugins.sourcemaps.init())
-		.pipe(plugins.postcss(processors))
-		.pipe(plugins.sourcemaps.write('.'))
-		.pipe(gulp.dest(config.src.dev.css.dest))
-		.on('end', browserSync.reload);
+	  .pipe(plugins.plumber({
+		  errorHandler: (err) => {
+			  console.log(err)
+		  }
+	  }))
+	  .pipe(plugins.sourcemaps.init())
+	  .pipe(plugins.sass())
+	  .pipe(plugins.postcss(processors))
+	  .pipe(plugins.sourcemaps.write('.'))
+	  .pipe(gulp.dest(config.src.dev.css.dest))
+	  .on('end', browserSync.reload);
 });
 
 var configHtml = {
@@ -114,32 +101,32 @@ var configHtml = {
 
 gulp.task('ajax', () => {
 	return gulp.src(config.src.source.ajax.src)
-		.pipe(plugins.plumber({
-			errorHandler: (err) => {
-				console.log(err)
-			}
-		}))
-		.pipe(plugins.nunjucksRender({
-			path: [config.src.source.components]
-		}))
-		.pipe(plugins.htmlPrettify(configHtml))
-		.pipe(gulp.dest(config.src.dev.ajax.dest))
+	  .pipe(plugins.plumber({
+		  errorHandler: (err) => {
+			  console.log(err)
+		  }
+	  }))
+	  .pipe(plugins.nunjucksRender({
+		  path: [config.src.source.components]
+	  }))
+	  .pipe(plugins.htmlPrettify(configHtml))
+	  .pipe(gulp.dest(config.src.dev.ajax.dest))
 
 });
 
 gulp.task('nunjucks', () => {
 	return gulp.src(config.src.source.html.src)
-		.pipe(plugins.plumber({
-			errorHandler: (err) => {
-				console.log(err)
-			}
-		}))
-		.pipe(plugins.nunjucksRender({
-			path: [config.src.source.components]
-		}))
-		.pipe(plugins.htmlPrettify(configHtml))
-		.pipe(gulp.dest(config.src.dev.dest))
-		.on('end', browserSync.reload);
+	  .pipe(plugins.plumber({
+		  errorHandler: (err) => {
+			  console.log(err)
+		  }
+	  }))
+	  .pipe(plugins.nunjucksRender({
+		  path: [config.src.source.components]
+	  }))
+	  .pipe(plugins.htmlPrettify(configHtml))
+	  .pipe(gulp.dest(config.src.dev.dest))
+	  .on('end', browserSync.reload);
 
 });
 
@@ -150,33 +137,33 @@ gulp.task('fonts', () => {
 				console.log(err)
 			}
 		}))
-		.pipe(gulp.dest(config.src.dev.fonts.dest));
+	  	.pipe(gulp.dest(config.src.dev.fonts.dest));
 });
 
 gulp.task('images', () => {
 
 	return gulp.src(config.src.source.images.src)
-		.pipe(plugins.plumber({
-			errorHandler: (err) => {
-				console.log(err)
-			}
-		}))
-		.pipe(plugins.cache(plugins.imagemin([
-			plugins.imagemin.gifsicle({
-				interlaced: true
-			}),
-			MozJpeg({
-				quality: 90
-			}),
-			plugins.imagemin.optipng({
-				optimizationLevel: 5
-			}),
-			plugins.imagemin.svgo({
-				plugins: [{removeViewBox: true}]
-			})
-		])))
-		.pipe(gulp.dest(config.src.dev.images.dest))
-		.on('end', browserSync.reload);
+	  .pipe(plugins.plumber({
+		  errorHandler: (err) => {
+			  console.log(err)
+		  }
+	  }))
+	  .pipe(plugins.cache(plugins.imagemin([
+		  plugins.imagemin.gifsicle({
+			  interlaced: true
+		  }),
+		  MozJpeg({
+			  quality: 90
+		  }),
+		  plugins.imagemin.optipng({
+			  optimizationLevel: 5
+		  }),
+		  plugins.imagemin.svgo({
+			  plugins: [{removeViewBox: true}]
+		  })
+	  ])))
+	  .pipe(gulp.dest(config.src.dev.images.dest))
+	  .on('end', browserSync.reload);
 });
 
 gulp.task('svg', () => {
@@ -186,8 +173,35 @@ gulp.task('svg', () => {
 				console.log(err)
 			}
 		}))
-		.pipe(plugins.cache(plugins.svgmin()))
+		.pipe(plugins.svgmin())
 		.pipe(gulp.dest(config.src.source.svg.dest));
+});
+
+// Basic configuration example
+var configSvg = {
+	mode: {
+		css: {
+			render: {
+				css: true
+			}
+		},
+		symbol: true
+	}
+};
+
+gulp.task('svgSprite', () => {
+	return gulp.src(config.src.source.svg.sprite.src, {cwd: ''})
+		.pipe(plugins.plumber({
+			errorHandler: (err) => {
+				console.log(err)
+			}
+		}))
+		.pipe(plugins.svgSprite(configSvg))
+				.on('error', function(error){
+			console.log(error)
+		})
+
+		.pipe(gulp.dest(config.src.source.svg.sprite.dest));
 });
 
 gulp.task('watch:nunjucks', () => {
@@ -242,4 +256,4 @@ gulp.task('watch', () => {
 });
 
 gulp.task('default', ['watch', 'webserver']);
-gulp.task('dev', ['svg', 'js', 'nunjucks', 'css', 'ajax', 'fonts', 'images']);
+gulp.task('dev', ['js', 'nunjucks', 'css', 'ajax', 'fonts', 'images']);
